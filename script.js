d41034b42f0ff93c4dd9b2f64f44516b521dc7ee
Kwena-cloud <53316152+Kwena-cloud@users.noreply.github.com>
2022-08-07 21:16:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// First lets create our drawing surface out of existing SVG element
+// If you want to create new surface just provide dimensions
+// like s = Snap(800, 600);
+var s = Snap("#Sudan");
+s.attr({
+    fill: "#fff"
+});
